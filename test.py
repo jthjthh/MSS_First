47866c80aaaeb85b4e0ace826ac120810fb65528
PercyGit <peng.cheng@my.jcu.edu.au>
2020-07-15 20:34:36 +0800

    加了一个教唆 test.py 的英雄
    加了一个英雄

---
+++ test.py
@@ -0,0 +1,25 @@
+"""
+You are required to look through xlwings Doc at:
+https://docs.xlwings.org/en/stable/quickstart.html
+in 5 minutes and give an output like the picture
+here: https://github.com/PercyCheng/Jacaranda-E/blob/master/William.png
+
+Parameter may be used:
+"Liam", "Noah", "William", "James", "Logan", "Benjamin", "Mason", "Elijah", "Oliver", "Jacob"
+1, 2, 3, 4, 5, 6, 7, 8, 9, 10
+
+
+You will have 10 minutes to finish your answer.
+
+Submission: Python file or text file are accepted when you submit your answer.
+You can submit directly with python file or text file using file name as "yourname_answer.py" or "yourname_answer.txt". eg: laowang_answer.py
+Please submit directly to “Genevievechen1996@hotmail.com”
+"""
+
+# TODO: import xlwings libarary
+
+
+# TODO: initialize sheets(Sheet name is “[your name] + "_answer"” for example "Bob_answer")
+
+
+# TODO: finish Your Code
